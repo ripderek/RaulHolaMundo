9f38da282e64d0513db35d8fcc53bd4ef4662241
ripderek <dquinteroa@uteq.edu.ec>
2023-06-08 16:16:39 -0500

    btn Enviar con mensaje

--- app/src/main/java/com/example/raulholamundo/MainActivity.java
+++ app/src/main/java/com/example/raulholamundo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     //Crear una funcion para el boton xd
     //Onlick para enviar desde el boton
     public void  btnEnviar(View view){
-        Toast.makeText(this.getApplicationContext(), "Click en boton", Toast.LENGTH_SHORT).show();
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        Toast.makeText(this.getApplicationContext(), "Hola "+ txtNombre.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
